32cb9bd7c55b033ee1b49318672e1aa8eddb0854
IsaSuzumura <isabelasuzumura@gmail.com>
2025-03-28 13:05:28 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./style.css"
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonNome, setNome] = useState("");
+
+  function loadAPI(){
+    let url = 'https://pokeapi.co/api/v2/pokemon/sylveon';
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json)
+      setPokemon(json)
+    })
+    .catch(err => console.log(err));
+  }
+
+  function searchPokemon(){
+    if(!pokemonNome) return;
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemonNome.toLowerCase()}`;
+
+    fetch(url)
+      .then(response => {
+        if(!response.ok) throw new Error("Pokemon NÃO encontrado");
+        return response.json();
+      })
+      .then(json => {
+        console.log(json);
+        setPokemon(json);
+      })
+      .catch(err => {
+        console.error(err);
+        alert("Pokemon não encontrado! Tente outro nome.")
+      });
+  }
+  useEffect(()=>{
+    loadAPI();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>  
+        <header>
+          <strong>Pokedex</strong>
+        </header>
+
+        <div>
+          <img src={pokemon.sprites?.front_default} alt={pokemon.name}/>
+          <div>Nome: {pokemon.name}</div>
+          <div>Nº {pokemon.id}</div>
+          <div>Peso: {pokemon.weight /10}kg</div>
+          <div>Altura: {pokemon.height /10}m</div>
+          <div>Tipo: {pokemon.types?.[0]?.type.name}</div>
+        </div>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Digite o nome do Pokemon"
+            value={pokemonNome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <button onClick={searchPokemon}>Buscar</button>
+        </div>
     </div>
   );
 }
